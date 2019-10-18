5c15f175b6d386954f85a7eda2754e6ed791f240
allison cortez <allizetorc@gmail.com>
2019-10-18 20:14:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,23 +1,20 @@
-def prime?(num)
-  (2..(num - 1)).each do |n|
-    false if num % n == 0 
-  end
-  true
-end
-
-
-# error: be sure to account for negative numbers
-
-
-# def prime6?(num)
-#   return true if num == 2
-#   return false if num <= 1 || num.even?
-#   i = 3
-#   top = Math.sqrt(num).floor
-#   loop do
-#     return false if (num % i).zero?
-#     i += 2
-#     break if i >= top
+# def prime?(num)
+#   (2..(num - 1)).each do |n|
+#     false if num % n == 0 
 #   end
 #   true
 # end
+
+
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
+    return true
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
+end
+
+  
