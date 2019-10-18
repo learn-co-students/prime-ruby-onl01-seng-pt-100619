d52da849aa0f7d89a46766cdfd3cb47fc4660f3b
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-18 20:52:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  binding.pry
+  start = 2
+    if number > 1
+      number_array = (start..number-1).to_a
+      number_array.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+  else
+    false 
+  end
+end
+
