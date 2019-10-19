609e0b2bbb134a80236c0e4ee62a2e94d5f8a138
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-10-19 04:14:53 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number <= 1 
+    return false
+  end
+  array = (1..number).to_a
+  array.each_with_index do |i|
+    remainder = number % i 
+    if remainder == 0 && i != number && i != 1
+      return false
+  end
+ end
+ return true 
+end  
+
+
+
