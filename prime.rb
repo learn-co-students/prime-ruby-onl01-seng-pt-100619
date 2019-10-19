b3959f0cbeba34431d47d01582033071c9197c6f
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 16:51:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+
+
+
+def prime?(integer)
+  integer = math.random ().to_a 
+  prime?.each do |number|
+   { n/1,n/2,n/3,n/5,n/n}
+    if {|number| n / n == 1}
+      "true, prime number"
+    else {|number| n / 3 == (integer), n/2 == (integer), n/5 == (integer)} 
+      "false, composite number"
+    end
+  end 
+  end 
+  
+  prime? 2 
+  prime?3
+  prime?11
+  prime?105557
+  
+  prime?-1
+  prime?0
+  prime?1
+  prime?4
+  prime?40
+  prime?1763
+  prime?101013
+  
