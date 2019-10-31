4676ddc1d6523888654b26d868b2a22af44f14a7
Ricky dildy <rdildy92@gmail.com>
2019-10-31 23:39:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+puts "Entire the number:"
+  num=gets.chomp. to_i 
+<1 
+puts "0 is not prime"
+    else 
+      
+       i = 2 
+       while(i<num)
+               if (num%i==0)
+                       count +=1 
+                     end 
+                     i+=1 
+                   end 
+                   
+                 end 
+                    if count>1 
+                      puts "#{num} is not a prime number"
+                  else 
+                    puts"#{num} is not a prime number"
+                  end 
+      
